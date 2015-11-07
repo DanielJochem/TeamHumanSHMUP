006e143076be61c06feda2875e7848efc55414c4
DanielJochem <daniel.jochem@gmail.com>
2015-11-07 21:13:43 +1000

    Final .gitignore fix?

--- Team Human SHMUP/Assets/Scripts/TestBackgroundScroll.cs
+++ Team Human SHMUP/Assets/Scripts/TestBackgroundScroll.cs
@@ -3,14 +3,21 @@ using System.Collections;
 
 public class TestBackgroundScroll : MonoBehaviour {
 
-    public float scrollSpeed = 0.1f;
-    public float scrollSpeed2 = 0.0f;
-    public Vector2 scroll;
+    public float scrollSpeed = 5.0f;
+    private Vector2 scroll;
 
-    void FixedUpdate()
-    {
-        scroll.x = Time.time * scrollSpeed2;
-        scroll.y = -(Time.time * scrollSpeed);
+    void Awake() {
+        scroll.x = 0;
+    }
+
+    void FixedUpdate()  {
+        //Increase the speed of the screen scrolling.
+        if ((Mathf.Abs(GetComponent<Renderer>().material.mainTextureOffset.y) % 1.0f) >= 0.004f) {
+            scrollSpeed += 0.002f;
+        }
+
+        scroll.y = -(Time.time * scrollSpeed) / 100.0f;
         GetComponent<Renderer>().material.mainTextureOffset = scroll;
+        Debug.Log(GetComponent<Renderer>().material.mainTextureOffset + " + " + (Mathf.Abs(GetComponent<Renderer>().material.mainTextureOffset.y) % 1.0f));
     }
 }
