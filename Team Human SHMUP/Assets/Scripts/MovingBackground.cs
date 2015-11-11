995b2208c184b4471637d45b0217abe9475d874f
DanielJochem <daniel.jochem@gmail.com>
2015-11-12 00:39:19 +1000

    Really cool Space Movement

--- Team Human SHMUP/Assets/Scripts/MovingBackground.cs
+++ Team Human SHMUP/Assets/Scripts/MovingBackground.cs
@@ -1,41 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.IO;
-using System.Collections.Generic;
 
 public class MovingBackground : MonoBehaviour {
 
-    public Texture[] frames;
-    //public FileInfo[] framesBefore;
-    //public List<FileInfo> frameArray;
-    public int framesPerSecond = 30;
+    public float scrollSpeed = 5.0f;
+    private Vector2 scroll;
 
-    //Was trying to get it automatically add the frames to the script, but I can't wuite get it to function, maybe you can try?
-    void Awake() {
-        //frames = (Texture[])Resources.LoadAll("Assets\\Images\\Background Frames\\*.gif");
+    void FixedUpdate()  {
+        //Increase the speed of the screen scrolling.
+        if ((Mathf.Abs(GetComponent<Renderer>().material.mainTextureOffset.y) % 1.0f) >= 0.004f) {
+            scrollSpeed += 0.002f;
+        }
 
-        /*
-        //DirectoryInfo dir = new DirectoryInfo("Assets\\Images\\Background Frames");
-        //framesToLoad = dir.GetFiles("*.gif");
-
-        for (int i = 0; i < framesToLoad.Length; i++) {
-             frameArray.Add(framesToLoad[i]);
-             frameArray.Sort();
-             frames[i] = (Texture)frameArray[i];
-             //frames[i] = framesToLoad[i];
-             Debug.Log((Texture)frameArray[i]);
-         }*/
-
-        //Debug.Log(frames.ToString());
+        scroll.y = -(Time.time * scrollSpeed) / 100.0f;
+        GetComponent<Renderer>().material.mainTextureOffset = scroll;
     }
-
-    void FixedUpdate() {
-        int index = (int)((Time.time / 1.1) * framesPerSecond) % frames.Length;
-        GetComponent<Renderer>().material.mainTexture = frames[index];
-    }
-
-
-
-
-
 }
